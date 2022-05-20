7f9a5c8cda2901803b4b82b58931cd37ebfa8fb9
Bartek <b.komorowski@intredo.com>
2022-05-20 11:37:10 +0200

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './components/*.{html,js,jsx}',
+    './public/index.html',
+    './src/**/*.js',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
